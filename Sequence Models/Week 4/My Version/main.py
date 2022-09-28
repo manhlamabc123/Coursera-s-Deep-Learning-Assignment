3b59275ee63556e5bb1edc939abd35718291ceb3
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-28 14:59:55 +0700

    Positional Encoding

--- Sequence Models/Week 4/My Version/main.py
+++ Sequence Models/Week 4/My Version/main.py
@@ -1,6 +1,22 @@
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.layers import Embedding, MultiHeadAttention, Dense, Input, Dropout, LayerNormalization
+# from keras.layers import Embedding, MultiHeadAttention, Dense, Input, Dropout, LayerNormalization
+from torch.nn import Embedding, MultiheadAttention, Dropout, LayerNorm
 from transformers import DistilBertTokenizerFast, TFDistilBertForTokenClassification #???
+from positional_encoding import *
+
+pos_encoding = positional_encoding(50, 512)
+
+print (pos_encoding.shape)
+
+plt.pcolormesh(pos_encoding[0], cmap='RdBu')
+plt.xlabel('d')
+plt.xlim((0, 512))
+plt.ylabel('Position')
+plt.colorbar()
+plt.show()
---
+++ Sequence Models/Week 4/My Version/positional_encoding.py
@@ -0,0 +1,38 @@
+import numpy as np
+# import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def get_angles(pos, k , d):
+
+    i = k // 2
+    angles = pos / (10000 ** (2 * i / d))
+
+    return angles
+
+def positional_encoding(positions, d):
+    """
+    Precomputes a matrix with all the positional encodings 
+    
+    Arguments:
+        positions (int) -- Maximum number of positions to be encoded 
+        d (int) -- Encoding size 
+    
+    Returns:
+        pos_encoding -- (1, position, d_model) A matrix with the positional encodings
+    """
+    # Initialize a matrix angle_ras of all the angles
+    angles_rads = get_angles(np.arange(positions)[:, np.newaxis], np.arange(d)[np.newaxis, :], d)
+
+    # Apply sin to even indices in the array; 2i
+    angles_rads[:, 0::2] = np.sin(angles_rads[:, 0::2])
+
+    # Apply cos to odd indices in the array; 2i+1
+    angles_rads[:, 1::2] = np.cos(angles_rads[:, 1::2])
+
+    pos_encoding = angles_rads[np.newaxis, ...]
+    
+    # return tf.cast(pos_encoding, dtype=tf.float32)
+    return torch.tensor(pos_encoding, dtype=torch.float32)
